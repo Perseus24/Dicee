c1597967d8c3e6441ec1fd4906a58ff0da7edb46
Cy Jay Herrera <herreracyjay24@gmail.com>
2024-01-16 11:05:57 +0800

    task(project): Challenge for printing a message

--- lib/main.dart
+++ lib/main.dart
@@ -23,16 +23,21 @@ class DicePage extends StatelessWidget {
         child: Row(
           children: [
             Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Image.asset('images/dice1.png'),
+              child: TextButton(
+                onPressed: (){
+                  print('Left button was pressed!');
+                },
+                child: Image.asset('images/dice1.png')
               )
             ),
             Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Image.asset('images/dice2.png'),
-              )
+              child: TextButton(
+                onPressed: (){
+                  print('Right button was pressed!');
+                },
+                child:  Image.asset('images/dice2.png'),
+              ),
+
             )
           ],
         ),
